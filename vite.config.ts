b686bad77091804ae83fe4aa5a993471ab522c9f
williamzuluaga-design <william.zuluaga@gmail.com>
2026-09-11 20:44:03 -0500

    Configure Vite for lab.cfinvestings.com
    Configure Vite for lab.cfinvestings.com

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,3 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  base: '/price-discovery-lab/',
+  base: '/',
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'node',
-  },
-});
